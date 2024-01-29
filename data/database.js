9c3da911759f955311b7bce3818cb3ecc24f3d56
zizou49 <ziedktari@outlook.fr>
2024-01-29 15:37:49 +0100

    correct Port mongo DB

--- data/database.js
+++ data/database.js
@@ -5,8 +5,9 @@ const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
+const dbPort = process.env.MONGODB_DB_PORT;
 
-const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}${dbPort}`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
